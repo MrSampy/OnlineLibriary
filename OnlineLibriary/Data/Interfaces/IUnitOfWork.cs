ac121ba51bc8c0a78f8b42515c246083b491eeb1
mynameisserzheo@gmail.com <mynameisserzheo@gmail.com>
2024-01-01 19:55:46 +0200

    Add interface IUnitOfWork

---
+++ OnlineLibriary/Data/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using Data.Entities;
+using Data.Repositories;
+
+namespace Data.Interfaces
+{
+    internal interface IUnitOfWork
+    {        
+        Repository<Author> AuthorRepository { get; }
+        Repository<Book> BookRepository { get; }
+        Repository<Genre> GenreRepository { get; }
+        Repository<User> UserRepository { get; }
+        public Task SaveAsync();
+    }
+}
