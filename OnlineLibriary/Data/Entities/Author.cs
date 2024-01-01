0e7c3ca3eea26756ac1234218ad2622934ae6f00
mynameisserzheo@gmail.com <mynameisserzheo@gmail.com>
2024-01-01 19:55:07 +0200

    Add entities

---
+++ OnlineLibriary/Data/Entities/Author.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Entities
+{
+    public class Author: BaseEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Country { get; set; }
+        public virtual ICollection<Book> Books { get; set; }
+    }
+}
---
+++ OnlineLibriary/Data/Entities/Genre.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Entities
+{
+    public class Genre: BaseEntity
+    {
+        public string Name { get; set; }
+        public virtual ICollection<Book> Books { get; set; }
+    }
+}
