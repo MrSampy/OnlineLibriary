ee3447706bc5cb31d070c0e13b0c319a683f72bb
mynameisserzheo@gmail.com <mynameisserzheo@gmail.com>
2024-01-10 23:54:49 +0200

    Add Username property and remove DateOfRegistration one from User entity

--- OnlineLibriary/Data/Entities/User.cs
+++ OnlineLibriary/Data/Entities/User.cs
@@ -8,11 +8,11 @@ namespace Data.Entities
 {
     public class User: BaseEntity
     {
+        public string Username { get; set; }
+        public string Password { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public DateTime DateOfRegistration { get; set; }
         public string Email { get; set; }
-        public string Password { get; set; }
         public byte[] ProfilePicture { get; set; }
         public virtual ICollection<Book> Books { get; set; }
     }
