6dcf9dd9a7fa4e346c25ab15f3ef3df7f0b7af05
mynameisserzheo@gmail.com <mynameisserzheo@gmail.com>
2024-01-10 23:52:27 +0200

    Add models

---
+++ OnlineLibriary/BusinessLogic/Models/DTOs/AuthorDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Models.DTOs
+{
+    public class AuthorDTO
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Country { get; set; }
+        public List<int> BookIds { get; set; }
+    }
+}
---
+++ OnlineLibriary/BusinessLogic/Models/PaginationModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Models
+{
+    public class PaginationModel
+    {
+        public int? PageNumber { get; set; }
+        public int? PageSize { get; set; }
+
+        public override string ToString()
+        {
+            return $"{PageNumber}:{PageSize}";
+        }
+    }
+}
