ad26df4cd49c988a26c31c5d3ddfa69e51dca877
mynameisserzheo@gmail.com <mynameisserzheo@gmail.com>
2024-01-10 23:52:41 +0200

    Add validators

---
+++ OnlineLibriary/BusinessLogic/Validation/AuthorValidator.cs
@@ -0,0 +1,132 @@
+﻿using BusinessLogic.Interfaces;
+using BusinessLogic.Models;
+using BusinessLogic.Models.DTOs;
+using Data.Entities;
+using Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Validation
+{
+    public class AuthorValidator : IValidator<AuthorDTO>
+    {
+        protected readonly IUnitOfWork _unitOfWork;
+        public AuthorValidator(IUnitOfWork unitOfWork) 
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<ValidationResult> ValidateForAddAsync(AuthorDTO model, bool identityCheck = true)
+        {
+            var result = new ValidationResult()
+            {
+                IsValid = true,
+                Messages = new List<string>()
+            };
+
+            if (string.IsNullOrWhiteSpace(model.FullName))
+            {
+                result.IsValid = false;
+                result.Messages.Add("Author full name cannot be empty");
+            }
+            else if(model.FullName.Split().Length != 2)
+            {
+                result.IsValid = false;
+                result.Messages.Add("Author full name must contain first and last name");
+            }
+            else if(model.FullName.Length > 100)
+            {
+                result.IsValid = false;
+                result.Messages.Add("Author full name cannot be longer than 100 characters");
+            }
+            else if(model.FullName.Length < 5)
+            {
+                result.IsValid = false;
+                result.Messages.Add("Author full name cannot be shorter than 5 characters");
+            }
+
+            if(model.DateOfBirth == default)
+            {
+                result.IsValid = false;
+                result.Messages.Add("Author date of birth cannot be empty");
+            }
+            else if(model.DateOfBirth > DateTime.Now)
+            {
+                result.IsValid = false;
+                result.Messages.Add("Author date of birth cannot be in the future");
+            }
+
+            if (string.IsNullOrWhiteSpace(model.Country))
+            {
+                result.IsValid = false;
+                result.Messages.Add("Author country cannot be empty");
+            }
+
+            if(model.BookIds != null && model.BookIds.Count != 0)
+            {
+                foreach (var bookId in model.BookIds)
+                {
+                    var book = await _unitOfWork.BookRepository.GetByIdAsync(bookId);
+                    if (book == null)
+                    {
+                        result.IsValid = false;
+                        result.Messages.Add($"Book with id {bookId} does not exist");
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public async Task<ValidationResult> ValidateForUpdateAsync(AuthorDTO model)
+        {
+           var result = new ValidationResult()
+           {
+                IsValid = true,
+                Messages = new List<string>()
+            };
+
+            if (model.Id == default)
+            {
+                result.IsValid = false;
+                result.Messages.Add("Author id cannot be empty");
+            }
+            else
+            {
+                var validationResult = await ValidateIdAsync(model.Id);
+                if (!validationResult.IsValid)
+                {
+                    result.IsValid = false;
+                    result.Messages.AddRange(validationResult.Messages);
+                }
+            }
+            var validationResultForAdd = await ValidateForAddAsync(model);
+            if (!validationResultForAdd.IsValid)
+            {
+                result.IsValid = false;
+                result.Messages.AddRange(validationResultForAdd.Messages);
+            }
+
+            return result;
+        }
+
+        public async Task<ValidationResult> ValidateIdAsync(int id)
+        {
+            var result = new ValidationResult()
+            {
+                IsValid = true,
+                Messages = new List<string>()
+            };
+            var author = await _unitOfWork.AuthorRepository.GetByIdAsync(id);
+            if(author == null)
+            {
+                result.IsValid = false;
+                result.Messages.Add($"Author with id {id} does not exist");
+            }
+            return result;
+        }
+    }
+}
---
+++ OnlineLibriary/BusinessLogic/Validation/BookValidator.cs
@@ -0,0 +1,177 @@
+﻿using BusinessLogic.Interfaces;
+using BusinessLogic.Models;
+using BusinessLogic.Models.DTOs;
+using Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Validation
+{
+    public class BookValidator : IValidator<BookDTO>
+    {
+        protected IUnitOfWork _unitOfWork;
+        public BookValidator(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<ValidationResult> ValidateForAddAsync(BookDTO model, bool identityCheck = true)
+        {
+            var result = new ValidationResult()
+            {
+                IsValid = true,
+                Messages = new List<string>()
+            };
+
+            if(string.IsNullOrWhiteSpace(model.Title))
+            {
+                result.IsValid = false;
+                result.Messages.Add("Book title cannot be empty");
+            }
+            else if(model.Title.Length > 100)
+            {
+                result.IsValid = false;
+                result.Messages.Add("Book title cannot be longer than 100 characters");
+            }
+            else if(model.Title.Length < 2)
+            {
+                result.IsValid = false;
+                result.Messages.Add("Book title cannot be shorter than 1 character");
+            }
+
+            if(model.AuthorId != default)
+            {
+                var author = await _unitOfWork.AuthorRepository.GetByIdAsync(model.AuthorId);
+                if(author == null)
+                {
+                    result.IsValid = false;
+                    result.Messages.Add($"Author with id {model.AuthorId} does not exist");
+                }
+            }
+
+            if(model.GenreId == default)
+            {
+                result.IsValid = false;
+                result.Messages.Add("Genre id cannot be empty");
+            }
+            else
+            {
+                var genre = await _unitOfWork.GenreRepository.GetByIdAsync(model.GenreId);
+                if(genre == null)
+                {
+                    result.IsValid = false;
+                    result.Messages.Add($"Genre with id {model.GenreId} does not exist");
+                }
+            }
+
+            if(model.Year == default)
+            {
+                result.IsValid = false;
+                result.Messages.Add("Publication year cannot be empty");
+            }
+            else if(model.Year < 0)
+            {
+                result.IsValid = false;
+                result.Messages.Add("Publication year cannot be negative");
+            }
+            else if(model.Year > DateTime.Now.Year)
+            {
+                result.IsValid = false;
+                result.Messages.Add("Publication year cannot be greater than current year");
+            }
+
+            if(string.IsNullOrWhiteSpace(model.Description))
+            {
+                result.IsValid = false;
+                result.Messages.Add("Book description cannot be empty");
+            }
+            else if(model.Description.Length > 1000)
+            {
+                result.IsValid = false;
+                result.Messages.Add("Book description cannot be longer than 1000 characters");
+            }
+            else if(model.Description.Length < 10)
+            {
+                result.IsValid = false;
+                result.Messages.Add("Book description cannot be shorter than 10 characters");
+            }
+
+            if (model.BookContent == null)
+            {
+                result.IsValid = false;
+                result.Messages.Add("Book content cannot be empty");
+            }
+            else if (model.BookContent.Length > 1000000)
+            {
+                result.IsValid = false;
+                result.Messages.Add("Book content cannot be longer than 1MB");
+            }
+
+            if(model.UserIds != null && model.UserIds.Any())
+            {
+                foreach (var userId in model.UserIds)
+                {
+                    var user = await _unitOfWork.UserRepository.GetByIdAsync(userId);
+                    if(user == null)
+                    {
+                        result.IsValid = false;
+                        result.Messages.Add($"User with id {userId} does not exist");
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public async Task<ValidationResult> ValidateForUpdateAsync(BookDTO model)
+        {
+            var result = new ValidationResult()
+            {
+                IsValid = true,
+                Messages = new List<string>()
+            };
+
+            if(model.Id == default)
+            {
+                result.IsValid = false;
+                result.Messages.Add("Book id cannot be empty");
+            }
+            else
+            {
+                var validationResult = await ValidateIdAsync(model.Id);
+                if(!validationResult.IsValid)
+                {
+                    result.IsValid = false;
+                    result.Messages.AddRange(validationResult.Messages);
+                }
+            }
+
+            var validationResultForAdd = await ValidateForAddAsync(model);
+            if(!validationResultForAdd.IsValid)
+            {
+                result.IsValid = false;
+                result.Messages.AddRange(validationResultForAdd.Messages);
+            }
+
+            return result;
+        }
+
+        public async Task<ValidationResult> ValidateIdAsync(int id)
+        {
+            var result = new ValidationResult()
+            {
+                IsValid = true,
+                Messages = new List<string>()
+            };
+            var author = await _unitOfWork.BookRepository.GetByIdAsync(id);
+            if (author == null)
+            {
+                result.IsValid = false;
+                result.Messages.Add($"Book with id {id} does not exist");
+            }
+            return result;
+        }
+    }
+}
